df8067552613d8b57e54bdaea092264e0a821051
Liam Townsley <DUFCLiam@users.noreply.github.com>
2016-04-08 20:16:40 +0100

    Update C_ruleinfo.java

--- src/me/serverinfo/commands/C_ruleinfo.java
+++ src/me/serverinfo/commands/C_ruleinfo.java
@@ -1,1 +1,29 @@
+package me.serverinfo.commands;
 
+import me.serverinfo.ServerInfo;
+import me.serverinfo.permissions.PermissionManager;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class C_ruleinfo implements CommandExecutor
+{
+
+    public C_ruleinfo(ServerInfo plugin)
+    {     
+    }
+
+    @Override
+    public boolean onCommand(CommandSender cs, Command cmnd, String string, String[] strings)
+    {
+        if (PermissionManager.hasPermission(cs, "serverinfo.default")) 
+        {
+	    Player p = (Player) cs;	
+	    p.sendMessage(ChatColor.BLUE + ServerInfo.plugin.getConfig().getString("rulesinfo"));
+	}
+        
+        return true;
+    }
+}
