d09c487bf1d232c0605e8bb28bb3ddbd2558a492
Liam Townsley <DUFCLiam@users.noreply.github.com>
2016-04-08 20:14:44 +0100

    Update ServerInfo.java

--- src/me/serverinfo/ServerInfo.java
+++ src/me/serverinfo/ServerInfo.java
@@ -1,42 +1,43 @@
 package me.serverinfo;
 
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+import me.serverinfo.commands.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class ServerInfo extends JavaPlugin 
-{
-	@Override
-	public void onEnable() {
-		getConfig().options().copyDefaults(true);
-		saveConfig();
-	}
-		//Made it less advanced for people who do not know how to code java
-		
-		public boolean onCommand(CommandSender sender, Command cmd, String Label, String[] args) {
-			if (cmd.getName().equalsIgnoreCase("website")) {
-				Player p = (Player)sender;	
-				p.sendMessage(ChatColor.BLUE + "Hello, " + p.getName() + " our website is ");
-				p.sendMessage(ChatColor.BLUE + getConfig().getString("web"));
-				//To change the color change the "BLUE" to anything
-			}
-			if (cmd.getName().equalsIgnoreCase("ai")) {
-				Player p = (Player)sender;	
-				p.sendMessage(ChatColor.BLUE + "Hello, " + p.getName() + " our website is ");
-				p.sendMessage(ChatColor.BLUE + getConfig().getString("admininfo"));
-			}
-			if (cmd.getName().equalsIgnoreCase("ruleinfo")) {
-				Player p = (Player)sender;	
-				p.sendMessage(ChatColor.BLUE + getConfig().getString("rulesinfo"));
-			}
-			if (cmd.getName().equalsIgnoreCase("owner")) {
-				Player p = (Player)sender;	
-				p.sendMessage(ChatColor.BLUE + getConfig().getString("owner"));
-			}
-			}
-			return false;
-	
-	}
+public class ServerInfo extends JavaPlugin {
+
+    public static ServerInfo plugin;
+
+    @Override
+    public void onLoad() {
+        ServerInfo.plugin = this;
+    }
+
+    @Override
+    public void onEnable() 
+    {
+        //Default load operation
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+        getLogger().info("ServerInfo has been enabled! (V2.0.0)");
+        
+        //Registering base command.
+        this.getCommand("admininfo").setExecutor(new C_admininfo(plugin));
+        this.getCommand("owner").setExecutor(new C_owner(plugin));
+        this.getCommand("ruleinfo").setExecutor(new C_ruleinfo(plugin));
+        this.getCommand("website").setExecutor(new C_website(plugin));
+        
+        //Registering aliases
+        this.getCommand("ai").setExecutor(new C_admininfo (plugin));
+        
+        //Usages
+        this.getCommand("admininfo").setUsage("/admininfo");
+        this.getCommand("ai").setUsage("/ai");
+    }
+
+    @Override
+    public void onDisable() 
+    {
+        saveConfig();
+        getLogger().info("ServerInfo has been disabled! (V2.0.0)");
+    }
 }
